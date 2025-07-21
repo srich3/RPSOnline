5596cfb4c9aea23c5e911be08e0eaba700b47bc1
srich3 <monsterchug@gmail.com>
2025-07-21 16:34:45 -0700

    chore: remove unused components, lib, and test pages

--- src/components/user/UsernameGate.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useAuth } from "../../hooks/useAuth";
-import { useUserStore } from "../../store/userStore";
-import { useEffect, useState } from "react";
-import { UsernamePrompt } from "./UsernamePrompt";
-
-export function UsernameGate({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-} 
